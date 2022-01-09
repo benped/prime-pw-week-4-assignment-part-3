dd7f8aa18a48068d3a98e9263eb1aa5ad1295b51
Ben Pedrick <bpedrick@gmail.com>
2022-01-09 14:15:56 -0600

    first stretch added

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,10 +2,15 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 let basket = [];
+const maxItems = 5;
 
 function addItem(item) {
-  basket.push(item);
-  return true;
+  if (isFull() === false) {
+    basket.push(item);
+    return true;
+  } else {
+    return false;
+  }
 }
 console.log(`Basket is currently ${basket}`)
 console.log(`Adding a kiwi: ${addItem('kiwi')}`);
@@ -24,6 +29,42 @@ function listItems(array) {
 console.log('Testing the listItems function:');
 listItems(basket);
 
+
 function empty() {
   basket = [];
 }
+
+
+// function empty(n) {
+//   n = [];
+// }
+// note to instructors - I can't figure out why building a function like this,
+// and passing the variable 'Basket' as an argument into this doesn't clear the
+// basket. Thoughts?
+
+empty();
+
+console.log(`This should be an empty basket: ${basket}`);
+
+
+
+// const maxItems = 5;
+// realizing this needs to be at the top for the function to run inside the
+// addItem function. Interesting!
+
+function isFull() {
+  if (basket.length < maxItems){
+    return false;
+  } else {
+    return true;
+  }
+}
+
+console.log(`Is the basket full? ${isFull(basket)}`);
+
+console.log(`Adding a kiwi: ${addItem('kiwi')}`);
+console.log(`Adding a apple: ${addItem('apple')}`);
+console.log(`Adding a cherry: ${addItem('cherry')}`);
+console.log(`Adding a banana: ${addItem('banana')}`);
+console.log(`Adding a orange: ${addItem('orange')}`);
+console.log(`Adding a apple: ${addItem('apple')}`);
